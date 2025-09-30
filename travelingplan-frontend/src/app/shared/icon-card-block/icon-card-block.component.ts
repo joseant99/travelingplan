e8a69266dc241817b9792964eeb54c67994159ee
joseant99 <35297910+joseant99@users.noreply.github.com>
2025-09-30 10:17:07 +0000

    icon lucide

--- travelingplan-frontend/src/app/shared/icon-card-block/icon-card-block.component.ts
+++ travelingplan-frontend/src/app/shared/icon-card-block/icon-card-block.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { NgFor } from '@angular/common';NgFor
+import { NgFor } from '@angular/common';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-icon-card-block',
   standalone: true,
-  imports: [MatIconModule, NgFor],
+  imports: [MatIconModule, NgFor, LucideAngularModule],
   templateUrl: './icon-card-block.component.html',
   styleUrls: ['./icon-card-block.component.css'],
 })
--- travelingplan-frontend/src/main.ts
+++ travelingplan-frontend/src/main.ts
@@ -4,10 +4,12 @@ import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { LucideAngularModule, Plane, Hotel, Map, Car, Utensils, TentTree } from 'lucide-angular';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(FormsModule)
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(LucideAngularModule.pick({ Plane, Hotel, Map, Car, Utensils, TentTree }))
   ]
 }).catch(err => console.error(err));
